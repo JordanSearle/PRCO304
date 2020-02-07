bdaccb46d4573a80f241b87e601b6e7cc18316a0
Jordan Searle <32457721+JordanSearle@users.noreply.github.com>
2020-02-07 14:11:50 +0000

    Added function names

--- classes/user.js
+++ classes/user.js
@@ -4,4 +4,34 @@ class user{
     #password;
     #email;
     #user_DOB;
-}
+    function validateDetails(){
+
+    }
+    function addUser(){
+
+    }
+    function delUser(){
+
+    }
+    function addGame() {
+
+    }
+    function addBookmark() {
+
+    }
+    function delBookmark() {
+
+    }
+    function addTag() {
+
+    }
+    function delTag() {
+
+    }
+    function addRating() {
+
+    }
+    function delRating() {
+
+    }
+  }
