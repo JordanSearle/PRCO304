f9b5e13ec33e8b5828ab799c2252542c765cd488
Jordan Searle <32457721+JordanSearle@users.noreply.github.com>
2020-02-07 14:57:49 +0000

    Added schemas file

--- classes/user.js
+++ classes/user.js
@@ -1,3 +1,4 @@
+var schemas = require("../schemas");
 class user{
     #userID;
     #username;
@@ -12,34 +13,69 @@ class user{
       this.#user_DOB = dob;
     }
     validateDetails(){
-      return false;
+      //Return true or false based on password and username values
+      var user = schemas.User;
+      user.findOne({
+        username: this.#username
+      }, function(err, obj) {
+        if (err) {
+          console.log(err);
+          res.status(404).send(err);
+        };
+        //If user doesn't exist
+        if (obj === null) {
+          return "username";
+        }
+        //If it find the item in the DB
+        else {
+          //if the username matches the password
+          if (obj.password == req.body.Password) {
+            //res.status(200).send(String(obj.userID));
+            //Login Stuff and session processes here.
+            return true;
+          }
+          //If the password is incorrect
+          else {
+            return "password";
+          }
+        }
+      });
+
     }
     addUser(){
+      //Create new user
 
     }
     delUser(){
-
+      //delete user
     }
     addGame() {
-
+      //add a new pending game
     }
     addBookmark() {
-
+      //add a new bookmark
     }
     delBookmark() {
-
+      //delete bookmark
     }
     addTag() {
-
+      //add bookmark tag
     }
     delTag() {
-
+      //remove bookmark tag
     }
     addRating() {
-
+      //add a game rating
     }
     delRating() {
-
+      //remove rating
+    }
+    read(){
+      //returns user details from database
+    }
+    //getters and setters
+    setPassword(password){
+      this.#password = password;
     }
   }
 module.exports.user = user;
--- test/class_test.js
+++ test/class_test.js
@@ -1,12 +1,19 @@
 const user = require('../classes/user');
 const expect  = require("chai").expect;
+var schemas = require("../schemas");
 
 describe('User Class',function() {
+  var acc = new user.user("sda","sda","sda","sda","sda");
   context("Testing Validate User", function(){
-    it('Password should return as false', function() {
-      var acc = new user();
+    it('Should return as true: details correct', function() {
+      var result = acc.validateDetails();
+      expect(result).to.equal(true);
+    })
+    it('Should return as false: details incorrect', function() {
+      acc.setPassword(null);
       var result = acc.validateDetails();
       expect(result).to.equal(false);
     })
+
   })
 })
