8ab7201e9a38d503e4cf24ed938218858e8d4bc2
Jordan Searle <32457721+JordanSearle@users.noreply.github.com>
2020-02-10 11:47:05 +0000

    Started to add setter test

--- test/class_test.js
+++ test/class_test.js
@@ -30,11 +30,45 @@ describe('User Class Getters and Setters',function() {
       expect(acc.getEmail()).to.not.equal("false");
     })
     it('Should return DOB as "Mon, 04 Dec 1995 00:12:00 GMT"', function() {
-      expect(acc.getDOB()).to.equal("04 Dec 1995 00:12:00 GMT");
+      expect(acc.getDOB()).to.equal("Mon, 04 Dec 1995 00:12:00 GMT");
     })
     it('Should return DOB as false: details incorrect', function() {
       expect(acc.getDOB()).to.not.equal("false");
     })
-
+  })
+  context("Testing Setters", function(){
+    it('Should set userID as "5"', function() {
+      acc.setUserID(5);
+      expect(acc.getUserID()).to.equal("5");
+    })
+    it('User ID should no longer = 1', function() {
+      expect(acc.getUserID()).to.not.equal("1");
+    })
+    it('Should set username as "UserFive"', function() {
+      acc.setUsername("UserFive");
+      expect(acc.getUsername()).to.equal("UserFive");
+    })
+    it('Username should no longer be UserOne', function() {
+      expect(acc.getUsername()).to.not.equal("UserOne");
+    })
+    it('Should set password as "MoreSecurePassword"', function() {
+      acc.setPassword("MoreSecurePassword");
+      expect(acc.getPassword()).to.equal("MoreSecurePassword");
+    })
+    it('password should no longer be password', function() {
+      expect(acc.getPassword()).to.not.equal("password");
+    })
+    it('Should return Email as "email@email.com"', function() {
+      expect(acc.getEmail()).to.equal("email@email.com");
+    })
+    it('Should return Email as false: details incorrect', function() {
+      expect(acc.getEmail()).to.not.equal("false");
+    })
+    it('Should return DOB as "Mon, 04 Dec 1995 00:12:00 GMT"', function() {
+      expect(acc.getDOB()).to.equal("Mon, 04 Dec 1995 00:12:00 GMT");
+    })
+    it('Should return DOB as false: details incorrect', function() {
+      expect(acc.getDOB()).to.not.equal("false");
+    })
   })
 })
