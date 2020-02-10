f7e63133ef8e39f6dc28b7d30277c789d62741b8
Jordan Searle <32457721+JordanSearle@users.noreply.github.com>
2020-02-10 11:31:35 +0000

    added more getter test

--- test/class_test.js
+++ test/class_test.js
@@ -8,8 +8,26 @@ describe('User Class Getters and Setters',function() {
     it('Should return username as "UserOne"', function() {
       expect(acc.getUsername()).to.equal("UserOne");
     })
+    it('Should return username as false: details incorrect', function() {
+      expect(acc.getUsername()).to.not.equal("false");
+    })
+    it('Should return password as "password"', function() {
+      expect(acc.getPassword()).to.equal("UserOne");
+    })
+    it('Should return password as false: details incorrect', function() {
+      expect(acc.getPassword()).to.not.equal("false");
+    })
+    it('Should return Email as "email@email.com"', function() {
+      expect(acc.getUsername()).to.equal("UserOne");
+    })
+    it('Should return Email as false: details incorrect', function() {
+      expect(acc.getUsername()).to.not.equal("false");
+    })
+    it('Should return password as "password"', function() {
+      expect(acc.getPassword()).to.equal("UserOne");
+    })
     it('Should return as false: details incorrect', function() {
-      expect(result).to.not.equal("false");
+      expect(acc.getPassword()).to.not.equal("false");
     })
 
   })
