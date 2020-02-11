a96113d513569a5b357de9d771f08a74ce68a672
Jordan Searle <32457721+JordanSearle@users.noreply.github.com>
2020-02-11 12:14:19 +0000

    Added game Schema

--- schemas.js
+++ schemas.js
@@ -9,7 +9,12 @@ var user = new Schema({
 var bookmark = new Schema({});
 var tag = new Schema({});
 var game = new Schema({
-
+  userID:{type: String, required: true, unique: true},
+  game_Name:{type: String, required: true},
+  game_Summery:{type: String, required: true},
+  game_Rules:{type: String, required: true},
+  game_Player_Count:{type: String, required: true},
+  game_Creator:{type: String, required: true},
 });
 var equipment = new Schema({});
 var rating = new Schema({});
