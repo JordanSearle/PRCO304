d9730822b0b37861fd9b21c814785c1cb3a11166
Jordan Searle <32457721+JordanSearle@users.noreply.github.com>
2020-02-11 12:12:11 +0000

    Added User Schema

--- schemas.js
+++ schemas.js
@@ -1,2 +1,18 @@
-var user = "blah";
+var mongoose = require('mongoose');
+var user = new Schema({
+  username:{type: String, required: true, unique: true},
+  password:{type: String, required: true},
+  email:{type: String, required: true, unique: true},
+  user_DOB:{type: Date, required: true},
+  isAdmin:{type: Boolean}
+});
+var bookmark = new Schema({});
+var tag = new Schema({});
+var game = new Schema({
+
+});
+var equipment = new Schema({});
+var rating = new Schema({});
+var pending = new Schema({});
+var pEquipment = new Schema({});
 module.exports.User = user;
