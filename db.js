a9cd9a7c14a8d20e30d916ca6f0457778db44c59
Jordan Searle <32457721+JordanSearle@users.noreply.github.com>
2020-02-12 13:41:54 +0000

    Added Server start query

---
+++ db.js
@@ -0,0 +1,1 @@
+const schemas = require('./schemas.js');
---
+++ server.js
@@ -0,0 +1,16 @@
+  var express = require('express');
+
+  //Server Start...
+  var server = app.listen(9000, function() {
+    // Connect to Mongoose.
+    mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }).then((test) => {
+      console.log("%s Connected to DB", new Date());
+    });
+    var port = server.address().port;
+    console.log('%s Listening on port %s', new Date(), port);
+  });
+
+  module.exports = server;
