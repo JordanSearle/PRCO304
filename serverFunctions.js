c803fd52cb993aa9c5c620ba192264d4fc96a9fe
jordansearle@msn.com <Marlie1997_>
2020-04-09 13:43:11 +0100

    Added basic export function headers

--- serverFunctions.js
+++ serverFunctions.js
@@ -1,26 +1,57 @@
 var classes = require('./classes');
-
+// Export the functions for each server operation, e.g Post game and get game
 module.exports = {
-
+//Unregistered User functions
+  default: function (req,res) {
+    // This it the app.get '/' function
+  },
+  login: function (req,res) {
+    // This it the app.post /login
+  },
+  user: function (req,res) {
+    // This is the app.post /createuser and /user, both are being moved into the same function using abstract factory pattern.
+  },
+  nextGame: function (ws,req) {
+    //This is the app.get /game/next
+  },
+  prevGame: function (ws,req) {
+    //This is the app.get /game/prev
+  },
+  randomGame: function (ws,req) {
+    //This is the app.get /game/random
+  },
+  //Logged on user Functions
+  logout: function (req,res) {
+    //This is the app.get /logout function
+  },
+  delUser: function (req,res) {
+    //This is the app.delete /user and /users/userID functions, both are being moved into the same function using abstract factory pattern.
+  },
+  editUser: function (req,res) {
+    //This is the app.put /user and /users/userID functions, both are being moved into the same function using abstract factory pattern.
+  },
+  getUser: function (req,res) {
+    //This is the app.get /user and /users/userID functions, both are being moved into the same function using abstract factory pattern.
+  }
 }
 function controllerFactory() {
     this.createUser = function (model) {
         var user;
 
         switch(model) {
-            case 'Cayman':
-            //User
-                car = new classes.user();
+            case false:
+            //logged on user level
+                user = new classes.user();
                 break;
-            case 'Boxster':
-            //Admin
-                car = new classes.admin();
+            case true:
+            //Admin user level
+                user = new classes.admin();
                 break;
             default:
-              //Redirect
+              //Default user level
                 break;
         }
 
-        return car;
+        return user;
     }
 }
